b0636260098de110d36371a832ee4a017e6154f0
jrslagle <jrslagle@gmail.com>
2021-02-28 16:19:54 -0700

    added a comment for where an app name appears

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from pages import index, predictions, insights, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
-    brand='YOUR APP NAME',
+    brand='YOUR APP NAME',  # appears in navigation bar
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
